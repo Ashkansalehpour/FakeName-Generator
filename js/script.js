f53edc51e79dcee840712c33c0a3350914476fbb
Ashkansalehpour <111193270+Ashkansalehpour@users.noreply.github.com>
2023-05-08 19:14:45 +0430

    creating selector for our script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// selectors
+// gender selector
+let genderSelector = document.querySelector("#gender-selector"),
+  // button selector
+  generatorBtn = document.querySelector("#generate-btn"),
+  // container selector
+  container = document.querySelector(".container"),
+  // body selector
+  body = document.querySelector("body");
